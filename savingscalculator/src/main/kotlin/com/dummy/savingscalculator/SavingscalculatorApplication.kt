0c324353fb501dd6a5c4a966405113f1ef822f2a
Denisse Landau <denisselandau97@gmail.com>
2023-04-26 11:04:01 -0300

    dummy project

---
+++ savingscalculator/src/main/kotlin/com/dummy/savingscalculator/SavingscalculatorApplication.kt
@@ -0,0 +1,11 @@
+package com.dummy.savingscalculator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SavingscalculatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<SavingscalculatorApplication>(*args)
+}
---
+++ savingscalculator/src/test/kotlin/com/dummy/savingscalculator/SavingscalculatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dummy.savingscalculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SavingscalculatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
